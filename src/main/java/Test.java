82f0c03eeb6587bbcdc0694c61b7ee818acc58ee
saeed <abdavi.saeed@gmail.com>
2019-07-28 10:32:00 +0430

    first commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+import ch.qos.logback.classic.LoggerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+
+    private static final Logger log = LoggerFactory.getLogger(Test.class);
+
+    public  static void main(String agrs[]) throws Exception{
+
+        log.info("TEST");
+    }
+
+}
